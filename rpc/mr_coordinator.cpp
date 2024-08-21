5d1cc865af29eade0b2b168b4fd03cdc80568cbc
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2024-08-20 21:36:55 -0600

    map part progress

--- rpc/mr_coordinator.cpp
+++ rpc/mr_coordinator.cpp
@@ -1,48 +1,95 @@
-#include <vector>
 #include <iostream>
-#include <map>
-#include <fstream>
-#include <sstream>
+#include <vector>
 #include <string>
 #include <grpcpp/grpcpp.h>
+#include "./mapreduce.grpc.pb.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
+using grpc::Status;
+using mapreduce::MapReduce;
+using mapreduce::MapRequest;
+using mapreduce::MapResponse;
 
+
+
+class MRCoordinator final : public MapReduce::Service {
+public:
+    Status mapCall(ServerContext* context, const MapRequest* req, MapResponse* res) override {
+        std::cout << "Processing here" << std::endl;
+        std::string worker = "Requesting worker was : " + req->worker_id();
+        std::string filename = filenames[process_value];
+        res->set_filename(filename);
+        res->set_process_id(process_value);
+        process_value++;
+        return Status::OK;
+    }
+
+private:
+    int process_value = 0;
+    std::vector<std::string> filenames = {"pg-being_ernest.txt", "pg-dorian_gray.txt", 
+            "pg-frankenstein.txt", "pg-grimm.txt", "pg-huckleberry_finn.txt", 
+            "pg-metamorphosis.txt", "pg-sherlock_holmes.txt", "pg-tom_sawyer.txt"};
+};
 
 void runServer() {
     std::string server_address("0.0.0.0:50051");
+    MRCoordinator service;
 
     ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
-    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
     if (!server) {
         throw std::runtime_error("Failed to create server");
     } else {
         std::cout << "Server listening on " << server_address << std::endl;
     }
+    server->Wait();
 }
 
-
-int main() {
-
-    // initialize server
+int main(int argc, char** argv) {
+    /*
+    int num_mappers = *argv[1];
+    int num_reducers = *argv[2];
+    */
+    
     runServer();
+    
 
-    std::vector<std::string> filenames = {"pg-being_ernest.txt", "pg-dorian_gray.txt", 
-            "pg-frankenstein.txt", "pg-grimm.txt", "pg-huckleberry_finn.txt", 
-            "pg-metamorphosis.txt", "pg-sherlock_holmes.txt", "pg-tom_sawyer.txt"};
 
+
+    /*
 
     
 
 
+    for (int i = 0; i < filenames.size(); i++) {
+        std::string* fname = &filenames[i]; // maybe not pointer? deference in rpc call?
+        // respond to workers with the filename
+    }
+
+    // shuffle and sort phase
+
+    // make bucket folders
+    std::vector<std::string> bucket_fnames;
+    for (int i = 0; i < (26 / num_reducers); i++) {
+        bucket_fnames.push_back("bucket_" + i);
+    }
+
+    // read in a file, parse it and assign items to their bucket
+    // do this by taking the first letter of the key, converting to a = 0 ascii, then modulo by num_reducers
+    for (int i = 0; i < filenames.size(); i++) {
+        std::string fname = "mr-" + i;
+        
+    }
+
+    */
+
     // do map
 
     // send request to coordinator for reduce function buckets
-
 
     return 0;
 }
--- rpc/mr_worker.cpp
+++ rpc/mr_worker.cpp
@@ -4,33 +4,95 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <grpcpp/grpcpp.h>
+#include "./mapreduce.grpc.pb.h"
 
-// map function
-std::map<std::string, int> mapf(const std::string & fname, std::ifstream & input) {
-    std::map<std::string, int> res;
-    std::string line;
-    std::string token;
-    while (getline(input, line)) {
-        std::istringstream data(line);
-        while (data >> token) {
-            res[token]++;
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using mapreduce::MapReduce;
+using mapreduce::MapRequest;
+using mapreduce::MapResponse;
+
+void mapf(const std::string & ifname, std::ifstream & input, std::ofstream & output);
+
+class MapClient {
+public:
+    MapClient(std::shared_ptr<Channel> channel) : stub_(MapReduce::NewStub(channel)) {}
+
+    void mapCall(const std::string worker_id) {
+        MapRequest request;
+        request.set_worker_id(worker_id);
+
+        MapResponse response;
+        ClientContext context;
+
+        Status status = stub_->mapCall(&context, request, &response);
+
+        if (status.ok()) {
+            std::cout << "Filename : " << response.filename() << std::endl;
+            std::cout << "Process ID : " << response.process_id() << std::endl;
+        } else {
+            std::cerr << "RPC failed: " << status.error_message() << std::endl;
         }
+
+        // call map on the filename 
+        std::string ifname;
+        ifname = response.filename();
+        std::ifstream input(ifname);
+        
+        std::string ofname;
+        ofname = "mr-" + worker_id + "-" + std::to_string(response.process_id()) + ifname; // only temporarily appending filename to check for race conditions on getting files (gdb says multithreaded server?)
+        std::ofstream output;
+        output.open(ofname);
+
+        mapf(ifname, input, output);
     }
 
-    return res;
+private:
+    std::unique_ptr<MapReduce::Stub> stub_;
+};
+
+// map function
+void mapf(const std::string & ifname, std::ifstream & input, std::ofstream & output) {
+    if (input.is_open()) {
+        std::map<std::string, int> res;
+        std::string line;
+        std::string token;
+        while (getline(input, line)) {
+            std::istringstream data(line);
+            while (data >> token) {
+                res[token]++;
+            }
+        }
+
+        // write to output file
+        if (output.is_open()) {
+            for (auto& pair : res) {
+                output << pair.first << " " << pair.second << std::endl;
+            }
+            output.close();
+        } else {
+            std::cerr << "Failed to open output file" << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to open input file " << std::endl;
+    }
 }
 
 
+/*
+    params:
+        [1] - worker_id
 
-int main() {
+*/
+int main(int argc, char** argv) {
+    /* int worker_id = *argv[1];
+    */
 
-    // send request to coordinator for map functions
-
-
-    // do map
-
-    // send request to coordinator for reduce function buckets
-
+    MapClient client(grpc::CreateChannel("0.0.0.0:50051",
+        grpc::InsecureChannelCredentials()));
+    client.mapCall("1");
 
     return 0;
 }
